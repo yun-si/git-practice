5109079caac0aa3d6af5644a91d16f77dfa63371
yunsi <110306079@g.nccu.edu.tw>
2024-09-26 14:20:24 +0800

    Add another way for fibonacci

--- week-02/fib.js
+++ week-02/fib.js
@@ -22,3 +22,22 @@ for (let j = 0; j < 10; j++) {
 }
 
 console.log(answer);
+
+// another way
+
+let fibonacci1 = (n) => {
+    let arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (let i=2; i<=n; i++) {
+        arr[n] = arr[n-1] + arr[n-2]
+    }
+    return arr[n]
+}
+
+let answer1 = []
+for (let j = 0; j < 10; j++) {
+    answer1.push(fibonacci(j));
+}
+
+console.log(answer1);
